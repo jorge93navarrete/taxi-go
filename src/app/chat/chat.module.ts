5c3c4fa10b18ccfad2efa21619d83e450b48c843
jorge-navarrete <jnavarrete@gbteknova.com>
2021-09-11 15:22:03 -0600

    chat module primeros pasos

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatViewComponent } from './chat-view/chat-view.component';
 import { ChatRoutingModule } from './chat-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { ChatInComponent } from './components/chat-in/chat-in.component';
 
 @NgModule({
-  declarations: [ChatViewComponent],
-  imports: [CommonModule, ChatRoutingModule],
+  declarations: [ChatViewComponent, ChatInComponent],
+  imports: [IonicModule, CommonModule, ChatRoutingModule],
 })
 export class ChatModule {}
